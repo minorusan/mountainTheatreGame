c8c7af1516c97adf2122df952f0ff336c5cbd86f
Niceguy Klim <klimshchukin1992@gmail.com>
2018-02-22 17:53:23 +0200

    Background appearence

---
+++ Assets/Scripts/Gameplay/Animation/BackgroundAppearenceBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+
+public class BackgroundAppearenceBehaviour : MonoBehaviour
+{
+	public event Action finishedMovement;
+
+	public float appearSpeed;
+	public float delay;
+	public float offsetX;
+	public Ease ease;
+
+	private void OnEnable()
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			var child = transform.GetChild (i);
+			var childPosition = child.localPosition;
+			var cachedX = childPosition.x;
+
+			childPosition.x += cachedX > 0 ? offsetX : -offsetX;
+			child.localPosition = childPosition;
+
+			child.DOLocalMoveX (cachedX, appearSpeed + (i * delay)).SetEase (ease);
+		}	
+	}
+}
